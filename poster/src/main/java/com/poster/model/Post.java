5af07166c95b71f0726fb1e195270e288c4aa90f
Vladilshk <vova.ilyshenko2016@gmail.com>
2023-06-29 16:46:49 +0300

    Add Post and User entities

---
+++ poster/src/main/java/com/poster/model/Post.java
@@ -0,0 +1,20 @@
+package com.poster.model;
+
+import jakarta.persistence.*;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
