375b57630400b66907a0276d049af5a5a1eafdb4
p.manilov <pavman0123456789@gmail.com>
2023-07-12 18:02:35 +0300

    Add post by followers method in controller, service and repository

--- poster/src/main/java/com/poster/repository/PostRepository.java
+++ poster/src/main/java/com/poster/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.poster.repository;
 
 import com.poster.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +13,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAll();
     Optional<Post> findById(Long Id);
     List<Post> findAllByUserId(Long user_id);
+    @Query("SELECT p FROM Post p WHERE p.user.id IN :following ORDER BY p.id DESC")
+    List<Post> findPostsByFollowing(@Param("following") List<Long> following);
 }
